8f71e4003f1e796140b0dc0b934c0b344ba42b81
AbulFozolJumman <abulfozoljumman@gmail.com>
2024-03-26 23:15:30 +0600

    Deploy React app to GitHub Pages

---
+++ src/assets/Installations.tsx
@@ -0,0 +1,31 @@
+// npm create vite@latest > React > Typescript > npm i > npm i -D tailwindcss postcss autoprefixer > npx tailwind init -p
+
+// tailwind.config.js
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//     content: [
+//       "./index.html",
+//       "./src/**/*.{js,ts,jsx,tsx}",
+//     ],
+//     theme: {
+//       extend: {},
+//     },
+//     plugins: [],
+//   }
+
+// index.css
+// @tailwind base;
+// @tailwind components;
+// @tailwind utilities;
+
+// npm run dev (Initial setup done)
+
+// For using ShadCN UI
+// tsconfig.json
+// After this line - "skipLibCheck": true,
+// "baseUrl": ".",
+// "paths": {
+//   "@/*": [
+//     "./src/*"
+//   ]
+// },
