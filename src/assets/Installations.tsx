c16a1ff24ba1ae97ec123a1951c3bbe0e739e637
AbulFozolJumman <abulfozoljumman@gmail.com>
2024-03-27 00:03:32 +0600

    shadcn ui added to the app

--- src/assets/Installations.tsx
+++ src/assets/Installations.tsx
@@ -29,3 +29,32 @@
 //     "./src/*"
 //   ]
 // },
+
+// npm i -D @types/node
+
+// vite.config.ts
+// import path from "path"
+// import react from "@vitejs/plugin-react"
+// import { defineConfig } from "vite"
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
+
+// npx shadcn-ui@latest init
+// Would you like to use TypeScript (recommended)? yes
+// Which style would you like to use? › Default
+// Which color would you like to use as base color? › Slate
+// Where is your global CSS file? › › src/index.css
+// Do you want to use CSS variables for colors? › yes
+// Where is your tailwind.config.js located? › tailwind.config.js
+// Configure the import alias for components: › @/components
+// Configure the import alias for utils: › @/lib/utils
+// Are you using React Server Components? › no
+
+// To use shadcn Button - npx shadcn-ui@latest add button
