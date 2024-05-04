a4a4b2e9b87cbc04a9245b4ee6ae711a1bb1b537
AbulFozolJumman <abulfozoljumman@gmail.com>
2024-05-04 23:48:33 +0600

    Website Title Updated

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+// import { useState } from "react";
 import AddTodoModal from "./AddTodoModal";
 import TodoFilter from "./TodoFilter";
-import TodoCard from "./todoCard";
+import TodoCard from "./TodoCard";
 
 const TodoContainer = () => {
-  const [todos, setTodos] = useState({ data: [] });
+  // const [todos, setTodos] = useState({ data: [] });
   return (
     <div>
       <div className="flex justify-between mb-5 ">
@@ -12,14 +12,17 @@ const TodoContainer = () => {
         <TodoFilter />
       </div>
       <div className="bg-primary-gradient w-full h-full rounded-xl  p-[5px]">
-        {/* <div className="bg-white p-5 w-full h-full rounded-lg space-y-3">
-          {todos?.data?.map((item) => (
+        <div className="bg-white p-5 w-full h-full rounded-lg space-y-3">
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          {/* {todos?.data?.map((item) => (
             <TodoCard {...item} />
-          ))}
-        </div> */}
-        <div className="bg-white text-2xl font-bold p-5 flex justify-center items-center rounded-md">
-          <p>There is no task pending</p>{" "}
+          ))} */}
         </div>
+        {/* <div className="bg-white text-2xl font-bold p-5 flex justify-center items-center rounded-md">
+          <p>There is no task pending</p>
+        </div> */}
       </div>
     </div>
   );
